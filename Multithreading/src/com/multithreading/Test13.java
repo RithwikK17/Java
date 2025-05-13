9355fd9de24d52183b6b9c834cf8c026b0c866f8
RithwikK17 <rithwik2603@gmail.com>
2025-05-13 13:11:02 +0530

    Deadlocks

---
+++ Multithreading/src/com/multithreading/Test13.java
@@ -0,0 +1,73 @@
+package com.multithreading;
+
+class InterthreadCommunication {
+	int i;
+	boolean flag = false;
+
+	public synchronized void deliverData(int i) {
+		if (flag) {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		this.i = i;
+		flag = true;
+		System.out.println("Data Delivered " + i);
+		notify();
+	}
+
+	public synchronized int receiveData() {
+		if (!flag) {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+		}
+		System.out.println("Data Received: " + i);
+		flag = false;
+		notify();
+		return i;
+	}
+}
+
+class DeliverThread extends Thread {
+	private InterthreadCommunication obj;
+
+	DeliverThread(InterthreadCommunication obj) {
+		this.obj = obj;
+	}
+
+	public void run() {
+		for (int j = 1; j <= 5; j++) {
+			obj.deliverData(j);
+		}
+	}
+}
+
+class ReceiverThread extends Thread {
+	private InterthreadCommunication obj;
+
+	public ReceiverThread(InterthreadCommunication obj) {
+		this.obj = obj;
+	}
+
+	public void run() {
+		for (int k = 0; k <= 5; k++) {
+			obj.receiveData();
+		}
+	}
+}
+
+public class Test13 {
+	public static void main(String[] args) {
+		InterthreadCommunication obj = new InterthreadCommunication();
+		DeliverThread t1 = new DeliverThread(obj);
+		ReceiverThread t2 = new ReceiverThread(obj);
+		t1.start();
+		t2.start();
+	}
+}
